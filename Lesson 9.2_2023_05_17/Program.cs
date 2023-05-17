f35f9a0a49546da06372664d50ae327575de6d39
С - 3 < - 3@DESKTOP-E0FTS21>
2023-05-17 22:32:16 +0300

    Добавьте файлы проекта.

---
+++ Lesson 9.2_2023_05_17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Lesson_9._2_2023_05_17
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = @"Data Source = test.db;";
+            var connection = new SQLiteConnection(path);
+            connection.Open();
+            Console.WriteLine($"Подключено " + $"{connection.ToString()}");
+            string query = @"CREATE TABLE IF NOT EXISTS Person(id INTEGER PRIMARY KEY AUTOINCREMENT"
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
